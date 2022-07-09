5d31189d4db29355215c88f5a47c9c778f6a6f2a
augusto <augustosaaude@gmail.com>
2022-07-09 12:50:01 -0300

    create context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import Modal from "react-modal";
+
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./context/TransactionsContext";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { NewTransactionModal } from "./components/NewTransactionModal";
+
 import { GlobalStyle } from "./styles/global";
 Modal.setAppElement('#root');
 
@@ -18,7 +21,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenTransactionModal} />
 
       <Dashboard />
@@ -29,6 +32,6 @@ export function App() {
       />
       
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
